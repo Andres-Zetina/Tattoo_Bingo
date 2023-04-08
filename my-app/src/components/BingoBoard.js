6e5c7fc4f19967d2da8a32bc054a247e306ef6a2
Andres-Zetina <zetinaandres700@gmail.com>
2023-04-08 15:55:52 -0400

    added save button

--- my-app/src/components/BingoBoard.js
+++ my-app/src/components/BingoBoard.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import TattooSquare from './TattooSquare';
-import '../styling/BingoBoard.css'
+import '../styling/BingoBoard.css';
 
-// Sample list of tattoo ideas
 const tattooIdeas = [
   'Skull',
   'Rose',
   'Anchor',
   'Dragon',
   'Heart',
-  // Add more ideas here
 ];
 
 function BingoBoard() {
   const [squares, setSquares] = useState(Array(25).fill(''));
   const [isEditable, setIsEditable] = useState(true);
+  const [savedBoards, setSavedBoards] = useState([]);
+  const [selectedSquares, setSelectedSquares] = useState(new Set());
+
+  const saveBoard = () => {
+    setSavedBoards([...savedBoards, squares.slice()]);
+  };
 
   const generateRandomBoard = () => {
     setIsEditable(false);
@@ -25,11 +29,19 @@ function BingoBoard() {
     setSquares(randomSquares);
   };
 
-  const handleSquareChange = (index, newValue) => {
+  const handleSquareChange = (index, newValue, isSelecting = false) => {
     if (isEditable) {
       const newSquares = squares.slice();
       newSquares[index] = newValue;
       setSquares(newSquares);
+    } else if (isSelecting) {
+      const newSelectedSquares = new Set(selectedSquares);
+      if (selectedSquares.has(index)) {
+        newSelectedSquares.delete(index);
+      } else {
+        newSelectedSquares.add(index);
+      }
+      setSelectedSquares(newSelectedSquares);
     }
   };
 
@@ -42,11 +54,29 @@ function BingoBoard() {
             index={index}
             value={square}
             isEditable={isEditable}
+            isSelected={selectedSquares.has(index)}
             onChange={handleSquareChange}
           />
         ))}
       </div>
       <button onClick={generateRandomBoard}>Randomize</button>
+      <button onClick={saveBoard}>Save</button>
+      <div className="saved-boards">
+        {savedBoards.map((savedBoard, idx) => (
+          <div key={idx} className="board">
+            {savedBoard.map((square, index) => (
+              <TattooSquare
+                key={index}
+                index={index}
+                value={square}
+                isEditable={false}
+                isSelected={false}
+                onChange={() => {}}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- my-app/src/components/TattooSquare.js
+++ my-app/src/components/TattooSquare.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../styling/TattooSquare.css';
 
-function TattooSquare({ index, value, isEditable, onChange }) {
+function TattooSquare({ index, value, isEditable, isSelected, onChange,}) {
+  const handleClick = () => {
+    onChange(index, value, true);
+  };
+
   const handleChange = (event) => {
     onChange(index, event.target.value);
   };
 
   return (
     <input
-      className="TattooSquare"
+      className={`TattooSquare ${isSelected ? 'selected' : ''}`}
       type="text"
       value={value}
       readOnly={!isEditable}
       onChange={handleChange}
+      onClick={isEditable ? null : handleClick}
     />
   );
 }
